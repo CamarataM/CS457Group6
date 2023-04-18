988b83b7285869619d43f54995dd0479039e90e4
CamarataM <98002889+CamarataM@users.noreply.github.com>
2023-04-18 19:50:11 +0000

    Add generate_minimal_complete_dataset(), dataset shuffling
    Added ability to generate a minimal dataset containing every possible combination of the 'I' and 'L' matrices. Similarly, added dataset shuffling which should be done on the dataset regardless as stated in today's lecture. Fixed a exception which would be caused in the number of folds was greater than the number of elements in the dataset.

--- Lab 1/li_perceptron.py
+++ Lab 1/li_perceptron.py
@@ -1,8 +1,8 @@
 # Taken From 04/16/2023 12:11 PM: https://machinelearningmastery.com/implement-perceptron-algorithm-scratch-python/
 # Perceptron Algorithm on the Sonar Dataset
 from math import sqrt
-from random import seed
-from random import randrange
+import random
+from typing import List
 
 # Split a dataset into k folds
 def cross_validation_split(dataset, n_folds):
@@ -12,7 +12,7 @@ def cross_validation_split(dataset, n_folds):
 	for i in range(n_folds):
 		fold = list()
 		while len(fold) < fold_size:
-			index = randrange(len(dataset_copy))
+			index = random.randrange(len(dataset_copy))
 			fold.append(dataset_copy.pop(index))
 		dataset_split.append(fold)
 
@@ -82,12 +82,12 @@ def perceptron(train, test, l_rate, n_epoch):
 
 	return(predictions)
 
-# Creates a linear array which contains either an 'I' or an 'L' depending on if 'l_tails' is greater than 0. 'left_shift' will shift the starting point for the 'I' or 'L' either left for negative values or right for positive values.
-def generate_dataset(width, l_tails = 0, left_shift = 0):
+# Creates a linear array which contains either an 'I' or an 'L' depending on if 'l_tails' is greater than 0. 'right_shift' will shift the starting point for the 'I' or 'L' either left for negative values or right for positive values.
+def generate_matrix(width : int, l_tails = 0, right_shift = 0):
 	dataset_max = (width * width) + 1
 	dataset = [0] * dataset_max
 
-	next_1_index = left_shift % width
+	next_1_index = right_shift % width
 	for i in range(dataset_max):
 		if next_1_index == 0:
 			dataset[i] = 1
@@ -108,7 +108,7 @@ def generate_dataset(width, l_tails = 0, left_shift = 0):
 	return dataset
 
 # Prints a dataset matrix in a more human-readable form.
-def print_dataset_matrix_form(dataset):
+def print_dataset_matrix_form(dataset : List[int]):
 	current_string = ""
 	wrap_modulus = int(sqrt(len(dataset) - 1))
 	for i in range(len(dataset)):
@@ -120,52 +120,102 @@ def print_dataset_matrix_form(dataset):
 
 	print(current_string)
 
-seed(1)
-
+random.seed(1)
 
 # Produced the greatest result with just plugging in random numbers for a bit. Anything less than 50 will produce about 80% accuracy.
-number_of_folds = 90
+number_of_folds = 100
 
 learning_rate = 0.01
 
 # TODO: Describe what this actually controls, not sure yet.
 number_of_epochs = 500
 
+# Controls the diameter (width and height) of the dataset matrices.
 matrix_width = 5
-dataset_size = 100
+
+# Controls if for random dataset generation if 'L' matrices should be checked for being valid or not. Since this is mathematically controlled, this is not technically necessary, but ensures that any strange behaviour is caught.
 verify_l_shape = True
 
-dataset = []
-for _ in range(dataset_size):
-	# Will randomly generate whether the dataset produced will be an 'I' or an 'L' by setting the amount of tails for the matrix to '0' or '1' respectively.
-	l_tails = randrange(0, 2)
-	is_l = l_tails > 0
+# Controls whether dataset order is shuffled or not.
+shuffle_dataset = True
 
-	max_shift = matrix_width
+# Set which value corresponds to which matrices for the truth values.
+I_MATRIX_TRUTH_VALUE = 0
+L_MATRIX_TRUTH_VALUE = 1
 
-	# If the matrix is an 'L' shape, the maximum shift we can do is the width of the matrix - 1, as anything more will cut the tail off.
-	if is_l:
-		max_shift = matrix_width - 1
+def generate_random_dataset(dataset_size : int):
+	dataset = []
 
-	# Generate a new dataset with a random shift between 0 and the max shift variable.
-	new_dataset_matrix = generate_dataset(matrix_width, l_tails, randrange(0, max_shift))
+	for _ in range(dataset_size):
+		# Will randomly generate whether the dataset produced will be an 'I' or an 'L' by setting the amount of tails for the matrix to '0' or '1' respectively.
+		l_tails = random.randrange(0, 2)
+		is_l = l_tails > 0
 
-	if verify_l_shape and is_l:
-		counted_l_tails = 0
-		for i in range(len(new_dataset_matrix)):
-			# Count the amount of 'L' tails the matrix list has. If this is equal to 0, then we have a malformed dataset array for the 'L' shape.
-			if new_dataset_matrix[i] == 1 and i + 1 < len(new_dataset_matrix) and new_dataset_matrix[i + 1] == 1:
-				counted_l_tails += 1
+		max_shift = matrix_width
 
-		# If the counted 'L' tails is not equal to the expected generated value, print an error and exit the program.
-		if counted_l_tails != l_tails:
-			raise Exception("Invalid 'L' dataset array, counted 'L' tails not equal to generated value (Counted: " + str(counted_l_tails) + ", Expected: " + str(l_tails) + "). Dataset: " + str(new_dataset_matrix))
+		# If the matrix is an 'L' shape, the maximum shift we can do is the width of the matrix - 1, as anything more will cut the tail off.
+		if is_l:
+			max_shift = matrix_width - 1
 
-	# Append to the end whether the resulting dataset is an 'I' or an 'L', required for the Perceptron to check whether the produced output was correct or not.
-	new_dataset_matrix.append(1 if is_l else 0)
+		# Generate a new dataset with a random shift between 0 and the max shift variable.
+		new_dataset_matrix = generate_matrix(matrix_width, l_tails, random.randrange(0, max_shift))
 
-	# Append the new dataset to the dataset list.
-	dataset.append(new_dataset_matrix)
+		if verify_l_shape and is_l:
+			counted_l_tails = 0
+			for i in range(len(new_dataset_matrix)):
+				# Count the amount of 'L' tails the matrix list has. If this is equal to 0, then we have a malformed dataset array for the 'L' shape.
+				if new_dataset_matrix[i] == 1 and i + 1 < len(new_dataset_matrix) and new_dataset_matrix[i + 1] == 1:
+					counted_l_tails += 1
+
+			# If the counted 'L' tails is not equal to the expected generated value, print an error and exit the program.
+			if counted_l_tails != l_tails:
+				raise Exception("Invalid 'L' dataset array, counted 'L' tails not equal to generated value (Counted: " + str(counted_l_tails) + ", Expected: " + str(l_tails) + "). Dataset: " + str(new_dataset_matrix))
+
+		# Append to the end whether the resulting dataset is an 'I' or an 'L', required for the Perceptron to check whether the produced output was correct or not.
+		new_dataset_matrix.append(L_MATRIX_TRUTH_VALUE if is_l else I_MATRIX_TRUTH_VALUE)
+
+		# Append the new dataset to the dataset list.
+		dataset.append(new_dataset_matrix)
+
+	return dataset
+
+def generate_minimal_complete_dataset():
+	dataset = []
+
+	# Generate all possible 'I' values, which is all 'I' shifted to the right 0 through matrix width amount of times.
+	for right_shift in range(matrix_width):
+		new_dataset_matrix = generate_matrix(matrix_width, 0, right_shift)
+
+		# Append a 0 to indicate that this is an 'I' matrix.
+		new_dataset_matrix.append(I_MATRIX_TRUTH_VALUE)
+
+		# Append the new dataset to the dataset list.
+		dataset.append(new_dataset_matrix)
+
+	# Generate all possible 'L' values, which is all 'L' shifted to the right 0 through matrix width minus one amount of times.
+	for right_shift in range(matrix_width - 1):
+		new_dataset_matrix = generate_matrix(matrix_width, 1, right_shift)
+
+		# Append a 1 to indicate that this is an 'L' matrix.
+		new_dataset_matrix.append(L_MATRIX_TRUTH_VALUE)
+		
+		# Append the new dataset to the dataset list.
+		dataset.append(new_dataset_matrix)
+
+	return dataset
+
+# Generates a dataset of randomly generated matrices, with (pseudo-)equal chances of being an 'I' or 'L'. The parameter passed is how many matrices to generate for the dataset.
+dataset = generate_random_dataset(100)
+
+# Generates a dataset which contains every possible 'I' and 'L'
+# dataset = generate_minimal_complete_dataset()
+
+if shuffle_dataset:
+	random.shuffle(dataset)
+
+if number_of_folds > len(dataset):
+	number_of_folds = len(dataset)
+	print("Reduced number of folds to be equal to the amount of elements in dataset, which is " + str(number_of_folds))
 
 # Evaluate the Perceptron algorithm passing in the generated dataset and the various parameters related to the Perceptron.
 scores = evaluate_algorithm(dataset, perceptron, number_of_folds, learning_rate, number_of_epochs)
